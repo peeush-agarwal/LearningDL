e3b41d8b18525aed04cf96a0dc37cb1744952728
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-02-29 10:31:30 +0530

    video

--- ComputerVision/aibootcamplect5.py
+++ ComputerVision/aibootcamplect5.py
@@ -2,7 +2,7 @@ import cv2
 import imutils
 import numpy as np
 
-img = cv2.imread(r'Data/dog.jpg')
+# img = cv2.imread(r'Data/dog.jpg')
 
 def image_proc_pipeline(img):
     resized = imutils.resize(img, height=500)
@@ -13,9 +13,25 @@ def image_proc_pipeline(img):
     edges = cv2.Canny(blurred, 75, 200)
     return resized, edges
 
-resized, result = image_proc_pipeline(img)
+if __name__ == '__main__':
+    img = cv2.imread(r'Data/dog.jpg')
+    image_proc_pipeline(img)
+    exit(0)
 
-cv2.imshow("resized", resized)
-cv2.imshow("edge detection", result)
-# cv2.imshow("cv2 resized", resized_cv2)
-cv2.waitKey(0)
+    cap = cv2.VideoCapture(0)
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+
+        if ret:
+            resized, result = image_proc_pipeline(frame)
+
+            cv2.imshow("resized", resized)
+            cv2.imshow("edge detection", result)
+            # cv2.imshow("cv2 resized", resized_cv2)
+            # cv2.waitKey(0)
+        else:
+            break
+    
+    cap.release()
+    cv2.destroyAllWindows()
