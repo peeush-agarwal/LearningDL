23782756329fb2ed6b80e7fa4af213fae8f1e87d
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-03-20 22:25:16 +0530

    Add function to concat video files and use 'mp4v' fourcc

--- ComputerVision/merge_videos.py
+++ ComputerVision/merge_videos.py
@@ -1,29 +1,42 @@
 import cv2
 import imutils
+import os
 
-video_files = [r'Data/Nature_clip2.mp4', r'Data/People.mp4']
+base_path = 'Data/'
+input_files = ['Nature_clip2.mp4', 'People.mp4']
+output_file = 'Nature_merge.mp4'
 
 frame_width = 640
 frame_height = 360
 out_fps = 25
-out = cv2.VideoWriter(r"Data/Nature_merge.avi", cv2.VideoWriter.fourcc('M','J','P','G'), out_fps, (frame_width,frame_height), 1)
 
-for idx, file in enumerate(video_files):
-    print(f'Processing {file}')
-    cap = cv2.VideoCapture(file)
-    
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-            
-        if ret:
-            resized = imutils.resize(frame, frame_width, frame_height)
-            cv2.imshow('frame', resized)
-            out.write(resized)
-            
-            cv2.waitKey(1)
-        else:
-            break
+def concat_video_files(base_path, input_files, output_file, frame_width, frame_height, out_fps):
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(os.path.join(base_path, output_file), fourcc, out_fps, (frame_width,frame_height))
+    # fourcc = cv2.VideoWriter.fourcc('M','J','P','G')
+    # out = cv2.VideoWriter(r"Data/Nature_merge.avi", fourcc, out_fps, (frame_width,frame_height), 1)
 
-    cap.release()
-out.release()
-cv2.destroyAllWindows()
+    for idx, file in enumerate(input_files):
+        file_path = os.path.join(base_path, file)
+        print(f'Processing {file_path}')
+        cap = cv2.VideoCapture(file_path)
+        
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+                
+            if ret:
+                resized = imutils.resize(frame, frame_width, frame_height)
+                cv2.imshow('frame', resized)
+                out.write(resized)
+                
+                cv2.waitKey(1)
+            else:
+                break
+
+        cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    concat_video_files(base_path, input_files, output_file, frame_width, frame_height, out_fps)
+    print('Program completed.')
